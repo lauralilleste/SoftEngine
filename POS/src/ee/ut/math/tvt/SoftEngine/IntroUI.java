dc3d6bdf7bc84179974be04f7169ac72eba4185c
SelenaL <selena.lubi@gmail.com>
2014-10-15 22:28:00 +0300

    Update IntroUI.java

--- POS/src/ee/ut/math/tvt/SoftEngine/IntroUI.java
+++ POS/src/ee/ut/math/tvt/SoftEngine/IntroUI.java
@@ -1,7 +1,12 @@
 package ee.ut.math.tvt.SoftEngine;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -14,6 +19,7 @@ import java.util.List;
 import java.util.Properties;
 
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -22,6 +28,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+
 
 
 
@@ -33,6 +48,7 @@ import org.apache.log4j.Logger;
 
 import com.jgoodies.looks.MicroLayout;
 import com.jgoodies.looks.windows.WindowsLookAndFeel;
+import com.sun.xml.internal.ws.api.server.Container;
 
 import ee.ut.math.tvt.SoftEngine.IntroUI;
 
@@ -46,21 +62,26 @@ public class IntroUI extends JFrame {
 	Properties prop3 = new Properties();
 	private void Frame() {
 		
-		
+        final JFrame jFrame = new JFrame();
+        
+        
+        final java.awt.Container contentPane = jFrame.getContentPane();
+        contentPane.setLayout(new GridLayout(3, 1));
+     
+       
 		String propFileName1 = "application.properties";
 		String propFileName2 = "version.properties";
-	
 		Properties prop1 = new Properties();
 		Properties prop2 = new Properties();
 		
 		// size & location
 		int width = 800;
-		int height = 600;
-		setSize(width, height);
-		setTitle("Intro");
+		int height = 500;
+		jFrame.setSize(width, height);
+		jFrame.setTitle("Intro");
 
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+		jFrame.setLocation((screen.width - width) / 2, (screen.height - height) / 2);
 
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -104,37 +125,57 @@ public class IntroUI extends JFrame {
 		InputStream inputStream3 = getClass().getClassLoader()
 				.getResourceAsStream(propFileName2);
 		
-		JPanel panel = new JPanel();
-	
-		try {
-			BufferedImage image = ImageIO.read(new File(prop1.getProperty("team_logo")));
-			ImageIcon image1 = new ImageIcon(image);
-			JLabel imagelabel = new JLabel(image1);
+		JPanel cpanel = new JPanel();
+		cpanel.setLayout(new FlowLayout());
 		
-		panel.add(imagelabel);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		JLabel team_name = new JLabel("Team name: "+prop1.getProperty("team_name"));
-		JLabel team_email = new JLabel("Team email: "+prop1.getProperty("team_email"));
-		JLabel team_leader = new JLabel("Team leader: "+prop1.getProperty("team_leader"));
+		JPanel panel = new JPanel();
+		 panel.setLayout(new GridLayout(6, 1));
+	         
+		 
+		
+		 JLabel team_name = new JLabel("Team name: "+prop1.getProperty("team_name"),JLabel.CENTER);
+		 team_name.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		    team_name.setBounds(10, 91, 79, 19);
+		 panel.add(team_name);
+		JLabel team_email = new JLabel("Team leader email: "+prop1.getProperty("team_email"),JLabel.CENTER);
+		 team_email.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		    team_email.setBounds(10, 91, 79, 19);
+		 panel.add(team_email);
+		 
+		JLabel team_leader = new JLabel("Team leader: "+prop1.getProperty("team_leader"),JLabel.CENTER);
+		 team_leader.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		    team_leader.setBounds(10, 91, 79, 19);
+		 panel.add(team_leader);
 		JLabel team_members = new JLabel("Team members: "
 				+ prop1.getProperty("team_member1") + ", "
 				+ prop1.getProperty("team_member2") + ", "
-				+ prop1.getProperty("team_member3"));
-		JLabel version = new JLabel("Versioon "
-				+ prop2.getProperty("build.major.number")+"."+prop2.getProperty("build.minor.number")+"."+prop2.getProperty("build.revision.number"));
-		panel.add(team_name);
-		panel.add(team_leader);
-		panel.add(team_email);
-		panel.add(team_name);
-		panel.add(team_members);
-		panel.add(version);
+				+ prop1.getProperty("team_member3"),JLabel.CENTER);
+		 team_members.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		    team_members.setBounds(10, 91, 79, 19);
+		 panel.add(team_members);
 		
 		
-		getContentPane().add(panel);
-		setVisible(true);
+		  try {
+				BufferedImage image = ImageIO.read(new File(prop1.getProperty("team_logo")));
+				ImageIcon image1 = new ImageIcon(image);
+				JLabel imagelabel = new JLabel(image1,JLabel.CENTER);
+			
+				 contentPane.add(imagelabel);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+
+		
+		
+		
+		contentPane.add(panel);
+		  JLabel version = new JLabel("Versioon "
+					+ prop2.getProperty("build.major.number")+"."+prop2.getProperty("build.minor.number")+"."+prop2.getProperty("build.revision.number"),JLabel.CENTER);
+			 contentPane.add(version);
+	     
+		jFrame.setVisible(true);
 	}
 
 }
