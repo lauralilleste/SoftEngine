9253caf592daa8222458338af9b44722afb8f07e
lauralilleste <lauralilleste@gmail.com>
2014-10-15 13:21:49 +0300

    loeb properties faile

--- POS/src/ee/ut/math/tvt/SoftEngine/IntroUI.java
+++ POS/src/ee/ut/math/tvt/SoftEngine/IntroUI.java
@@ -10,13 +10,21 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.jgoodies.looks.windows.WindowsLookAndFeel;
 
 public class IntroUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public IntroUI() {
 		String propFileName1 = "application.properties";
-		Properties prop = new Properties();
+		String propFileName2 = "version.properties";
+		Properties prop1 = new Properties();
+		Properties prop2 = new Properties();
 		// size & location
 		int width = 600;
 		int height = 400;
@@ -32,17 +40,56 @@ public class IntroUI extends JFrame {
 				System.exit(0);
 			}
 		});
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName1);
 		try {
-			if (inputStream == null) {
-				throw new FileNotFoundException("property file '" + propFileName1 + "' not found in the classpath");
+			UIManager.setLookAndFeel(new WindowsLookAndFeel());
+
+		} catch (UnsupportedLookAndFeelException e1) {
+			// log.warn(e1.getMessage());
+		}
+
+		// Application
+		InputStream inputStream1 = getClass().getClassLoader()
+				.getResourceAsStream(propFileName1);
+		try {
+			if (inputStream1 == null) {
+				throw new FileNotFoundException("property file '"
+						+ propFileName1 + "' not found in the classpath");
 			}
-			prop.load(inputStream);
+			prop1.load(inputStream1);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		System.out.println(prop);
-		
+
+		// Version
+		InputStream inputStream2 = getClass().getClassLoader()
+				.getResourceAsStream(propFileName2);
+		try {
+			if (inputStream2 == null) {
+				throw new FileNotFoundException("property file '"
+						+ propFileName2 + "' not found in the classpath");
+			}
+			prop2.load(inputStream2);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		JPanel panel = new JPanel();
+		JLabel team_name = new JLabel(prop1.getProperty("team_name"));
+		JLabel team_email = new JLabel(prop1.getProperty("team_email"));
+		JLabel team_leader = new JLabel(prop1.getProperty("team_leader"));
+		JLabel team_members = new JLabel("Team members: "
+				+ prop1.getProperty("team_member1") + ", "
+				+ prop1.getProperty("team_member2") + ", "
+				+ prop1.getProperty("team_member3"));
+		JLabel version = new JLabel("Versioon "
+				+ prop2.getProperty("revision")+"."+prop2.getProperty("minor")+"."+prop2.getProperty("major"));
+		panel.add(team_name);
+		panel.add(team_leader);
+		panel.add(team_email);
+		panel.add(team_members);
+		panel.add(version);
+
+		getContentPane().add(panel);
 		setVisible(true);
 	}
 }
