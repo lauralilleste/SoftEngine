fa77599139fd4d5f39571bf610dadaed24f453be
SelenaL <selena.lubi@gmail.com>
2014-10-15 18:45:48 +0300

    Update Intro.java

--- POS/src/ee/ut/math/tvt/SoftEngine/Intro.java
+++ POS/src/ee/ut/math/tvt/SoftEngine/Intro.java
@@ -1,7 +1,15 @@
 package ee.ut.math.tvt.SoftEngine;
 
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.SoftEngine.Intro;
 public class Intro {
-	public static void main(String[] args) {
+	private static final Logger log = Logger.getLogger(Intro.class);
+	public static void main(String[] args) throws IOException {	
+		BasicConfigurator.configure();
 		@SuppressWarnings("unused")
 		IntroUI abc = new IntroUI();
 
