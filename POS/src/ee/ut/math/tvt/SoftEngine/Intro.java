097592a09f0a57bb46c922d747f0683235499c82
Selena Lubi <selena.lubi@gmail.com>
2014-10-16 13:11:48 +0300

    salessystem

--- POS/src/ee/ut/math/tvt/SoftEngine/Intro.java
+++ POS/src/ee/ut/math/tvt/SoftEngine/Intro.java
@@ -6,13 +6,41 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 import ee.ut.math.tvt.SoftEngine.Intro;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 	public static void main(String[] args) throws IOException {	
 		BasicConfigurator.configure();
-		new IntroUI();
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
 		
 
 	}
