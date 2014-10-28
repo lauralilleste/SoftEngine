5f1afdbf950dad26c4d279666f36d7265519733a
kerlikuningas <kuningaskerli@gmail.com>
2014-10-28 23:56:14 +0200

    Warehouse täiustatud

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
@@ -1,23 +1,40 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import net.miginfocom.swing.MigLayout;
+
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 
 public class StockTab {
 
   private JButton addItem;
-
+  private JButton confirm;
+  private JFrame addItemFrame;
+  private JPanel addItemPanel;
   private SalesSystemModel model;
+  private JTextField IdField;
+  private JTextField NameField;
+  private JTextField PriceField;
+  private JTextField QuantityField;
 
   public StockTab(SalesSystemModel model) {
     this.model = model;
@@ -58,7 +75,7 @@ public class StockTab {
     gc.anchor = GridBagConstraints.NORTHWEST;
     gc.weightx = 0;
 
-    addItem = new JButton("Add");
+    addItem = createAddItemButton();
     gc.gridwidth = GridBagConstraints.RELATIVE;
     gc.weightx = 1.0;
     panel.add(addItem, gc);
@@ -67,8 +84,102 @@ public class StockTab {
     return panel;
   }
 
+  
+  
+  // Create "Add" button, to add new items
+  private JButton createAddItemButton() {
+	  JButton b = new JButton("Add");
+	  b.addActionListener(new ActionListener(){
+		  public void actionPerformed(ActionEvent ae){
+			  addItemButtonClicked();
+		  }
+	  });
+	  return b;
+  }
+  // Create "Confirm" button, to confirm adding items
+  	private JButton createConfirmButton(){
+  		JButton b = new JButton("Confirm");
+  		b.addActionListener(new ActionListener(){
+  			public void actionPerformed(ActionEvent ae){
+  				confirmButtonClicked();
+  			}
+  			
+  		});
+  		return b;
+  }
+  
+  // Event handler for the "add" button
+  protected void addItemButtonClicked(){
+	  try{
+		  addItemBox();
+	  }
+	  catch(Exception e){
+		  JOptionPane.showMessageDialog(null, "Incorrect input","Warning",JOptionPane.WARNING_MESSAGE);
+		  addItemFrame.dispose();
+	  }
+  }
+  
+  Long id= (long) 1;
+  String name = null;
+  double price =0.0;
+  String desc =null;
+  int quantity = 0;
+  
+  // Event handler for the "confirm" button
+  protected void confirmButtonClicked(){
+	  try{
+		  id = Long.parseLong(IdField.getText());
+		  name = NameField.getText();
+		  price = Double.parseDouble(PriceField.getText());
+		  quantity = Integer.parseInt(QuantityField.getText());
+		  model.getWarehouseTableModel().addItem(new StockItem(id, name, desc, price, quantity));
+		  
+	  }
+	  catch(Exception e){
+		  JOptionPane.showMessageDialog(null, "Incorrect input","Warning",JOptionPane.WARNING_MESSAGE);
+		  addItemFrame.dispose();
+	  }
+  }
+  
+  //Add new items
+  private void addItemBox(){
+	  addItemPanel = new JPanel(new MigLayout("nogrid"));
+	  
+	  addItemFrame = new JFrame("Add item");
+	  addItemFrame.setSize(new Dimension(350,180));
+	  addItemFrame.setLocationRelativeTo(null);
+	  addItemFrame.setVisible(false);
+	  addItemFrame.add(addItemPanel);
+	  addItemFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	  
+	  IdField=new JTextField(6);
+	  NameField=new JTextField(6);
+	  PriceField=new JTextField(6);
+	  QuantityField=new JTextField(6);
+	  
 
-  // table of the wareshouse stock
+	  
+	  // item id
+	  addItemPanel.add(new JLabel("Id: "),"newline");
+	  addItemPanel.add(IdField);
+	  // item name
+	  addItemPanel.add(new JLabel("Name: "),"newline");
+	  addItemPanel.add(NameField);
+	  // item price
+	  addItemPanel.add(new JLabel("Price: "),"newline");
+	  addItemPanel.add(PriceField);
+	  //item amount
+	  addItemPanel.add(new JLabel("Quantity: "),"newline");
+	  addItemPanel.add(QuantityField);
+	  
+	  confirm = createConfirmButton();
+	  addItemPanel.add(confirm,"newline");
+	  
+	  addItemFrame.setVisible(true);
+	  
+	  
+  }
+  // table of the warehouse stock
   private Component drawStockMainPane() {
     JPanel panel = new JPanel();
 
@@ -91,5 +202,6 @@ public class StockTab {
     panel.setBorder(BorderFactory.createTitledBorder("Warehouse status"));
     return panel;
   }
-
 }
+
+
