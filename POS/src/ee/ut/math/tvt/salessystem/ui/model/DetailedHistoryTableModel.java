dd7b4c508785628a5110f1e8ca214fdda8559b0e
kerlikuningas <kuningaskerli@gmail.com>
2014-10-29 10:23:40 +0200

    2 asja juurde

--- POS/src/ee/ut/math/tvt/salessystem/ui/model/DetailedHistoryTableModel.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/model/DetailedHistoryTableModel.java
@@ -1,5 +1,11 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
-public class DetailedHistoryTableModel {
+import org.apache.log4j.Logger;
 
+public class DetailedHistoryTableModel {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(HistoryTableModel.class);
+	
+	
 }
