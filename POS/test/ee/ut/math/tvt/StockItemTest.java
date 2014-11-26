ec207ca96b11812f2e31d355b1537133c44a7501
Selena Lubi <selena.lubi@gmail.com>
2014-11-26 16:02:19 +0200

    build ja test

--- POS/test/ee/ut/math/tvt/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/StockItemTest.java
@@ -12,10 +12,27 @@ private StockItem item;
 
 @Before
 public void setUp() {
-  item = new StockItem((long) 1, "Viru Valge", "vodka", 3.50); 
+  item = new StockItem( (long) 27, "Laua Viin", "vodka", 3.50); 
 }
 
 
 
+@Test
+public void testClone() {
+	StockItem clone = (StockItem) item.clone();
 
+	assertEquals(item.getId(), clone.getId());
+	assertEquals(item.getName(), clone.getName());
+	assertEquals(item.getDescription(), clone.getDescription());
+	assertEquals(item.getPrice(), clone.getPrice(), 0.0001);
+	assertEquals(item.getQuantity(), clone.getQuantity());
+}
+
+@Test
+public void testGetColumn() {
+	assertEquals(item.getId(), item.getColumn(0));
+	assertEquals(item.getName(), item.getColumn(1));
+	assertEquals(item.getPrice(), item.getColumn(2));
+	assertEquals(item.getQuantity(), item.getColumn(3));
+}
 }
